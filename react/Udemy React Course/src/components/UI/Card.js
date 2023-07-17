1d6ebc10d1fd6b212bb14c8d387227a21d6cf1df
paul <paulentz@gmail.com>
2023-07-16 23:59:20 -0500

    moved react course files

---
+++ react/Udemy React Course/src/components/UI/Card.js
@@ -0,0 +1,11 @@
+import "./Card.css";
+
+export default function Card(props) {
+  const classes = "card " + props.className;
+
+  //wrapper component
+  return <div className={classes}>{props.children}</div>;
+}
+
+//children is a reserved nam will be the conent of the opening and
+//closing tags of the custom content

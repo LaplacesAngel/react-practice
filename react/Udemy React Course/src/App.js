3cb6df3f349d0a47bd161df9b0ccede6d1707e25
paul <paulentz@gmail.com>
2023-07-24 22:51:39 -0500

    expense form done

--- react/Udemy React Course/src/App.js
+++ react/Udemy React Course/src/App.js
@@ -1,4 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
+import ExpenseForm from "./components/NewExpense/ExpenseForm";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -25,6 +27,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense></NewExpense>
       <Expenses items={expenses}></Expenses>
     </div>
   );
